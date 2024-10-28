fe51ccefccb78897a46c2518eba6a202231c5007
yahya-salhi <pcclub10@gmail.com>
2024-10-28 16:32:11 +0100

    finished le 28/10

--- save_Gaza/src/components/WestBankSummary.jsx
+++ save_Gaza/src/components/WestBankSummary.jsx
@@ -1,5 +1,12 @@
 /* eslint-disable react/prop-types */
 import styles from "./WestBankSummary.module.css";
+import {
+  FaChild,
+  FaUserShield,
+  FaAmbulance,
+  FaHeartBroken,
+} from "react-icons/fa";
+
 function WestBankSummary({ westBank, isLoading }) {
   if (isLoading) return <p>Loading...</p>;
   if (!westBank || !westBank.killed || !westBank.injured)
@@ -10,22 +17,27 @@ function WestBankSummary({ westBank, isLoading }) {
   return (
     <div className={styles.statisticsContainer}>
       <div className={styles.statisticItem}>
+        <FaUserShield className={styles.icon} />
         <span className={styles.statisticValue}>{settler_attacks}</span>
         <span className={styles.statisticLabel}>Settler Attacks</span>
       </div>
       <div className={styles.statisticItem}>
+        <FaHeartBroken className={styles.icon} />
         <span className={styles.statisticValue}>{killed.total}</span>
         <span className={styles.statisticLabel}>Total Killed</span>
       </div>
       <div className={styles.statisticItem}>
+        <FaChild className={styles.icon} />
         <span className={styles.statisticValue}>{killed.children}</span>
         <span className={styles.statisticLabel}>Children Killed</span>
       </div>
       <div className={styles.statisticItem}>
+        <FaAmbulance className={styles.icon} />
         <span className={styles.statisticValue}>{injured.total}</span>
         <span className={styles.statisticLabel}>Total Injured</span>
       </div>
       <div className={styles.statisticItem}>
+        <FaChild className={styles.icon} />
         <span className={styles.statisticValue}>{injured.children}</span>
         <span className={styles.statisticLabel}>Children Injured</span>
       </div>
